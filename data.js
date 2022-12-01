7fe97018b65a108c38bab594333d03887adbb39d
KevinL96 <kevin.loja9078@utc.edu.ec>
2022-12-01 18:07:55 -0500

    Agregado Entrada Principal v1

--- data.js
+++ data.js
@@ -122,7 +122,7 @@ var APP_DATA = {
     },
     {
       "id": "3-a-piso3",
-      "name": "a-piso3",
+      "name": "Tercer Piso",
       "levels": [
         {
           "tileSize": 256,
@@ -157,12 +157,334 @@ var APP_DATA = {
         }
       ],
       "infoHotspots": []
+    },
+
+    // Data for Principal Floor
+    {
+      "id": "0-a-principal",
+      "name": "Entrada Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8425615421891326,
+          "pitch": 0.10016173034858689,
+          "rotation": 1.5707963267948966,
+          "target": "1-a-principal1"
+        },
+        {
+          "yaw": -0.8091014396309042,
+          "pitch": 0.06972686724212807,
+          "rotation": 4.71238898038469,
+          "target": "6-a-principal6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-a-principal1",
+      "name": "Entrada Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9763303462486483,
+          "pitch": 0.05318841717593514,
+          "rotation": 4.71238898038469,
+          "target": "0-a-principal"
+        },
+        {
+          "yaw": 1.701834820679009,
+          "pitch": 0.13620644927336478,
+          "rotation": 14.137166941154074,
+          "target": "2-a-principal2"
+        },
+        {
+          "yaw": 1.0922104149642813,
+          "pitch": 0.06952300113774257,
+          "rotation": 0,
+          "target": "3-a-principal3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-a-principal2",
+      "name": "Entrada Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1142547505836404,
+          "pitch": 0.08203307166468932,
+          "rotation": 6.283185307179586,
+          "target": "3-a-principal3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-a-principal3",
+      "name": "Entrada Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7227563805948769,
+          "pitch": 0.06282622496448909,
+          "rotation": 0,
+          "target": "0-a-principal"
+        },
+        {
+          "yaw": 0.8907353554729855,
+          "pitch": 0.09651233851354135,
+          "rotation": 0,
+          "target": "4-a-principal4"
+        },
+        {
+          "yaw": -1.9280797266362946,
+          "pitch": 0.09003374774838235,
+          "rotation": 0,
+          "target": "2-a-principal2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-a-principal4",
+      "name": "Entrada Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.370980678355542,
+          "pitch": 0.05092304700662709,
+          "rotation": 0,
+          "target": "5-a-principal5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-a-principal5",
+      "name": "Entrada Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1249688109162896,
+          "pitch": 0.08735379759574258,
+          "rotation": 0,
+          "target": "6-a-principal6"
+        },
+        {
+          "yaw": -1.2618903130679122,
+          "pitch": 0.08896491349373115,
+          "rotation": 0,
+          "target": "4-a-principal4"
+        },
+        {
+          "yaw": 0.41194348271706716,
+          "pitch": 0.08879258401587897,
+          "rotation": 0,
+          "target": "0-a-principal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-a-principal6",
+      "name": "Entrada Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5347626251947819,
+          "pitch": 0.0770218778181917,
+          "rotation": 0,
+          "target": "4-a-principal4"
+        },
+        {
+          "yaw": 1.1049320120295931,
+          "pitch": 0.12568085617165536,
+          "rotation": 0,
+          "target": "0-a-principal"
+        },
+        {
+          "yaw": -1.1950520609472441,
+          "pitch": 0.07528734373003232,
+          "rotation": 4.71238898038469,
+          "target": "5-a-principal5"
+        }
+      ],
+      "infoHotspots": []
     }
   ],
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": true,
     "viewControlButtons": true
   }
